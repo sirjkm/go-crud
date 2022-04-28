ef0afed15425108a41b2cbfa6feaf0985ca36bcb
Jason Manning <jasonkylemanning@gmail.com>
2022-04-27 23:49:33 -0400

    add structs

--- main.go
+++ main.go
@@ -10,14 +10,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Movie {
+type Movie struct {
 	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
 }
 
-type Director {
-
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname`
 }
 
 func main() {
-
+	
 }
