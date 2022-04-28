b28bcd8561292e3d93de49e9537aefbddd59b093
Jason Manning <jasonkylemanning@gmail.com>
2022-04-27 22:45:57 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie {
+	ID string `json:"id"`
+}
+
+type Director {
+
+}
+
+func main() {
+
+}
